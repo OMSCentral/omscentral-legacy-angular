720ec5a006b3c5833852516fc2d3aee2e7d4460c
Matt Martz <matt.martz@gmail.com>
2017-11-22 23:40:19 -0500

    migration script and review table basic

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as jsonData from '../../../merged-dev.json';
 
 @Component({
   selector: 'oms-reviews',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent implements OnInit {
+  courses = [];
 
-  constructor() { }
+  constructor() {
+    this.courses = Object.keys((<any>jsonData).courses).map(function (courseId) {
+      const course = (<any>jsonData).courses[courseId];
+      course.numReviews = Object.keys(course.reviews).length;
+      course.id = courseId;
+      return course;
+    });
+  }
 
   ngOnInit() {
   }
--- src/app/reviews/reviews.module.ts
+++ src/app/reviews/reviews.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReviewsComponent } from './reviews.component';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   declarations: [ReviewsComponent]
 })
