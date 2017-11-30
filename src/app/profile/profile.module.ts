c8584f426b41157449fe855766efc58a0ee53e75
Matt Martz <mmartz@aamc.org>
2017-11-30 16:02:28 -0500

    very rough profile page

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
+import { ClarityModule } from 'clarity-angular';
+import { ReviewsModule } from '../reviews/reviews.module';
+import { UserService } from '../core/user.service';
+import { ReviewService } from '../reviews/review.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ClarityModule,
+    ReviewsModule
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent],
+  providers: [ UserService, ReviewService ]
 })
 export class ProfileModule { }
