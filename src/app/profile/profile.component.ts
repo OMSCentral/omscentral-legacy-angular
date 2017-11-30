105e608a483c78b7f3df10cbc4237cdd33ad067f
Matt Martz <mmartz@aamc.org>
2017-11-30 16:02:28 -0500

    very rough profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../firebase/auth.service';
+import { UserService } from '../core/user.service';
+import { Observable } from 'rxjs/Observable';
+import { ReviewService } from '../reviews/review.service';
 
 @Component({
   selector: 'oms-profile',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  auth: any = {};
+  user$: Observable<any>;
+  reviews$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService, private reviewService: ReviewService) {}
 
   ngOnInit() {
+    this.user$ = this.userService.getUser();
+    this.user$.subscribe(user => {
+      if (user.reviews) {
+        console.log(user.reviews);
+        const reviewIds = Object.keys(user.reviews).filter(revId => {
+          return user.reviews[revId];
+        });
+        this.reviews$ = this.reviewService.getReviews(reviewIds);
+      }
+    });
   }
 
 }
