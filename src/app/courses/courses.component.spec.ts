158e92d74e517fc7d7893d049e7e8783a205cf28
Matt Martz <matt.martz@gmail.com>
2017-12-13 22:35:45 -0500

    fix tests

--- src/app/courses/courses.component.spec.ts
+++ src/app/courses/courses.component.spec.ts
@@ -1,6 +1,28 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ClarityModule } from 'clarity-angular';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CoursesComponent } from './courses.component';
+import { PipeModule } from '../pipes/pipe.module';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { CourseService } from '../core/course.service';
+class MockCourse {
+  getCourses() {
+    return new BehaviorSubject(null);
+  }
+}
+
+import { GradeService } from '../grades/grade.service';
+class MockGrade {
+  getCourses() {
+
+  }
+  getGrades() {
+
+  }
+}
 
 describe('CoursesComponent', () => {
   let component: CoursesComponent;
@@ -8,9 +30,14 @@ describe('CoursesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoursesComponent ]
+      imports: [PipeModule, ClarityModule, RouterTestingModule],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      providers: [{ provide: CourseService, useClass: MockCourse }, { provide: GradeService, useClass: MockGrade }],
+      declarations: [CoursesComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
