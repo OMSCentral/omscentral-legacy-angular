a5579552c89e90968e68bdfae988644b537fef41
Matt Martz <matt.martz@gmail.com>
2017-12-28 22:09:11 -0500

    admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertService } from '../core/alert/alert.service';
+import { CourseService } from '../courses/course.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
@@ -10,14 +11,18 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 })
 export class AdminComponent implements OnInit {
   alertForm: FormGroup;
+  courseForm: FormGroup;
   alerts$: BehaviorSubject<any>;
   alert: any;
+  course: any;
 
-  constructor(private alertService: AlertService, private fb: FormBuilder) {
+  constructor(private courseService: CourseService, private alertService: AlertService, private fb: FormBuilder) {
     this.initAlertForm();
+    this.initCourseForm();
   }
 
   ngOnInit() {
+    this.alerts$ = this.alertService.alerts$;
   }
 
   initAlertForm() {
@@ -27,13 +32,34 @@ export class AdminComponent implements OnInit {
     });
     this.alertForm.valueChanges.subscribe(changes => {
       this.alert = changes;
-      console.log(changes);
     });
   }
 
-  save() {
+  initCourseForm() {
+    this.courseForm = this.fb.group({
+      department: ['', Validators.required],
+      foundational: '',
+      name: ['', Validators.required],
+      number: ['', Validators.required],
+      program: ['', Validators.required]
+    });
+    this.courseForm.valueChanges.subscribe(changes => {
+      this.course = changes;
+    });
+  }
+
+  saveAlert() {
     this.alertService.addAlert(this.alert);
     this.initAlertForm();
+  }
+
+  saveCourse() {
+    this.courseService.push(this.course);
+    this.initCourseForm();
+  }
+
+  removeAlert(alert) {
+    this.alertService.removeAlert(alert.id);
   }
 
 }
--- src/app/core/alert/alert.service.ts
+++ src/app/core/alert/alert.service.ts
@@ -7,12 +7,16 @@ export class AlertService {
   alerts$: BehaviorSubject<any> = new BehaviorSubject(null);
 
   constructor(private db: AngularFireDatabase) {
-    this.db.database.ref('/alerts').on('value', snapshot => {
+    this.db.database.ref('/alerts').orderByChild('active').equalTo(1).on('value', snapshot => {
       const alertObj = snapshot.val();
       if (alertObj !== null) {
-        this.alerts$.next(Object.keys(alertObj).forEach(alert => {
-          return alertObj[alert].active;
+        this.alerts$.next(Object.keys(alertObj).map(alertId => {
+          const alert = alertObj[alertId];
+          alert.id = alertId;
+          return alert;
         }));
+      } else {
+        this.alerts$.next([]);
       }
     });
   }
@@ -21,13 +25,14 @@ export class AlertService {
     this.db.database.ref('/alerts').push({
       type: alert.type || 'info',
       text: alert.text || '',
-      slack: alert.slack || false,
-      active: true
+      slack: alert.slack ? 1 : 0 || 0,
+      created: new Date().getTime(),
+      active: 1
     });
   }
 
-  disableAlert(alertId) {
-    this.db.database.ref('/alerts/' + alertId + '/active').set(false);
+  removeAlert(alertId) {
+    this.db.database.ref('/alerts/' + alertId).remove();
   }
 
 }
