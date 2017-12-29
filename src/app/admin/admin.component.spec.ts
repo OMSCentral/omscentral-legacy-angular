6b16d37d00594f3908cbc07f282c8f99f2d2ac8e
Matt Martz <matt.martz@gmail.com>
2017-12-28 23:10:40 -0500

    fix tests

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ClarityModule } from 'clarity-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminComponent } from './admin.component';
+
+import { CourseService } from '../courses/course.service';
+class MockCourse { }
+
+import { AlertService } from '../core/alert/alert.service';
+class MockAlert { }
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,9 +16,14 @@ describe('AdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      imports: [ClarityModule, FormsModule, ReactiveFormsModule],
+      providers: [
+        { provide: CourseService, useClass: MockCourse },
+        { provide: AlertService, useClass: MockAlert }
+      ],
+      declarations: [AdminComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
