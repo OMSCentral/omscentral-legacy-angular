1e4176c506ff732c8709e4430109ab18314a6526
Matt Martz <matt.martz@gmail.com>
2018-01-06 16:38:30 -0500

    testing and linting

--- src/app/reviews/review-list/review-list.component.spec.ts
+++ src/app/reviews/review-list/review-list.component.spec.ts
@@ -1,6 +1,11 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReviewListComponent } from './review-list.component';
+import { PipeModule } from '../../pipes/pipe.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ClarityModule } from 'clarity-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('ReviewListComponent', () => {
   let component: ReviewListComponent;
@@ -8,9 +13,14 @@ describe('ReviewListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ReviewListComponent ]
+      imports: [PipeModule, RouterTestingModule, ClarityModule, FormsModule, ReactiveFormsModule],
+      schemas: [
+        NO_ERRORS_SCHEMA,
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      declarations: [ReviewListComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
