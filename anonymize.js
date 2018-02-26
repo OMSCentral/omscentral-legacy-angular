b66dc162d14cc00fc0551576fe90370065d2e527
Matt Martz <matt.martz@gmail.com>
2018-02-25 19:10:10 -0500

    update anon

--- anonymize.js
+++ anonymize.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var _ = require("lodash");
-var rawCurrent = fs.readFileSync("./backups/20180107.json");
+var rawCurrent = fs.readFileSync("./backups/2018-02-25T00_15_28Z_gt-surveyor_data.json");
 var current = JSON.parse(rawCurrent);
 
 var anon = _.cloneDeep(current);
