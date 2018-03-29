0059445f508d9112740e85d79387d7feef4119b6
Matt Martz <matt.martz@gmail.com>
2018-03-29 11:20:58 -0400

    fixed tests

--- src/app/courses/course/course.component.spec.ts
+++ src/app/courses/course/course.component.spec.ts
@@ -1,4 +1,40 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ClarityModule } from 'clarity-angular';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { PipeModule } from '../../pipes/pipe.module';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { CourseService } from '../../courses/course.service';
+class MockCourse {
+  getCourses() {
+    return new BehaviorSubject(null);
+  }
+  getCourse() {
+    return new BehaviorSubject({});
+  }
+}
+
+import { GradeService } from '../../grades/grade.service';
+class MockGrade {
+  getCourses() {
+
+  }
+  getGrades() {
+
+  }
+}
+
+import { ReviewService } from '../../reviews/review.service';
+class MockReview {
+
+}
+
+import { AuthService } from '../../firebase/auth.service';
+class MockAuth {
+  user = new BehaviorSubject({});
+}
 
 import { CourseComponent } from './course.component';
 
@@ -8,6 +44,14 @@ describe('CourseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [PipeModule, ClarityModule, RouterTestingModule],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
+      providers: [{ provide: CourseService, useClass: MockCourse },
+        { provide: GradeService, useClass: MockGrade },
+        { provide: AuthService, useClass: MockAuth },
+        { provide: ReviewService, useClass: MockReview }],
       declarations: [ CourseComponent ]
     })
     .compileComponents();
