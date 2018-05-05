3ba571d57db8b3bc48dfb61aae429982e820af8b
Matt Martz <matt.martz@gmail.com>
2018-05-05 00:46:33 -0400

    more material

--- src/app/donate/donate.module.ts
+++ src/app/donate/donate.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DonateComponent } from './donate.component';
 import { MarkdownModule } from 'angular2-markdown/markdown/markdown.module';
+import {
+  MatSidenavModule,
+  MatCardModule,
+  MatListModule,
+  MatButtonModule,
+} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, MarkdownModule],
+  imports: [
+    CommonModule,
+    MarkdownModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+  ],
   declarations: [DonateComponent],
 })
 export class DonateModule {}
