8b551b1ea73a8b6c51d3e8b5b79cbfdc359dc4d2
Matt Martz <matt.martz@gmail.com>
2018-05-05 00:46:33 -0400

    more material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { AlertComponent } from './alert/alert.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { GithubButtonModule } from 'angular-github-buttons';
+import { MatToolbarModule, MatButtonModule, MatRippleModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, AppRoutingModule, GithubButtonModule],
+  imports: [CommonModule, AppRoutingModule, GithubButtonModule, MatToolbarModule, MatButtonModule, MatRippleModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [NavComponent, SidebarComponent, AlertComponent],
   declarations: [NavComponent, SidebarComponent, AlertComponent],
