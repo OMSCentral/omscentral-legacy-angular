3f963f2304126c398f06878ccd1d9e54b0392f68
Matt Martz <matt.martz@gmail.com>
2018-05-05 22:08:58 -0400

    courses page

---
+++ src/app/models/course.ts
@@ -0,0 +1,16 @@
+export interface CourseData {
+    id: string;
+    name: string;
+    department: string;
+    foundational: boolean;
+    number: string;
+    combined: string;
+    enrolled: number;
+    reviews: number;
+    work: number;
+    difficulty: number;
+    rating: number;
+    ab: number;
+    cdf: number;
+    withdrew: number;
+  }
