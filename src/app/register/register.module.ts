fdce6e03010bb3cb6ed688576ddd7abed1259f91
Matt Martz <matt.martz@gmail.com>
2018-05-28 19:57:56 -0400

    basic auth skinning

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,10 +2,24 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { AuthService } from '../firebase/auth.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatCardModule,
+  MatInputModule,
+  MatDividerModule,
+  MatButtonModule,
+  MatFormFieldModule,
+} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [RegisterComponent],
   providers: [AuthService],
