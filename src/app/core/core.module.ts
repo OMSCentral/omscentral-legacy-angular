cd66eeb1d80b8523184fcda8ea4c407743fb4822
Matt Martz <matt.martz@gmail.com>
2018-05-29 20:38:07 -0400

    themed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,15 +2,32 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AlertComponent } from './alert/alert.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { GithubButtonModule } from 'angular-github-buttons';
-import { MatToolbarModule, MatButtonModule, MatRippleModule } from '@angular/material';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatRippleModule,
+  MatSnackBarModule,
+} from '@angular/material';
+import { AlertService } from './alert.service';
+import { LocalStorageService } from './local-storage.service';
+import { UserService } from './user.service';
+import { SettingsService } from './settings.service';
 
 @NgModule({
-  imports: [CommonModule, AppRoutingModule, GithubButtonModule, MatToolbarModule, MatButtonModule, MatRippleModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    GithubButtonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatSnackBarModule,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  exports: [NavComponent, SidebarComponent, AlertComponent],
-  declarations: [NavComponent, SidebarComponent, AlertComponent],
+  exports: [NavComponent, SidebarComponent],
+  declarations: [NavComponent, SidebarComponent],
+  providers: [AlertService, LocalStorageService, UserService, SettingsService],
 })
 export class CoreModule {}
