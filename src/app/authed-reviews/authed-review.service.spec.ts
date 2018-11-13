9b46935c6c7d8ccf9b9c76f3e46bf80ca1ea58dd
martzcodes <matt.martz@gmail.com>
2018-11-12 21:43:51 -0500

    sync

--- src/app/authed-reviews/authed-review.service.spec.ts
+++ src/app/authed-reviews/authed-review.service.spec.ts
@@ -5,11 +5,14 @@ import { AuthedReviewService } from './authed-review.service';
 describe('AuthedReviewService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthedReviewService]
+      providers: [AuthedReviewService],
     });
   });
 
-  it('should be created', inject([AuthedReviewService], (service: AuthedReviewService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject([AuthedReviewService], (service: AuthedReviewService) => {
+      expect(service).toBeTruthy();
+    })
+  );
 });
--- src/app/core/settings.service.spec.ts
+++ src/app/core/settings.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { SettingsService } from './settings.service';
 
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase } from '@angular/fire/database';
 class MockAFDB {}
 
 describe('SettingsService', () => {
